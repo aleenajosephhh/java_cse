94e236740356c1a1a5d955b4b565ccca1aac7db6
aleenajosephhh <aleenajsp123@gmail.com>
2025-03-13 10:59:49 +0530

    this is else statement

---
+++ IfElse/Else.java
@@ -0,0 +1,12 @@
+public class Else{
+    public static void main(String args[]){
+        if(10>=2){
+
+        
+        System.out.println(true);
+        }
+        else
+        
+            System.out.println(false);
+    }
+}
