49df9ac69c6f703de9bf469cd71c8feecbdd83aa
aleenajosephhh <aleenajsp123@gmail.com>
2025-03-13 11:31:05 +0530

    to check whether the given number is positive or negative

---
+++ PositiveNegative/Positive.java
@@ -0,0 +1,12 @@
+public class Positive{
+    public static void main(String args[]){
+        int x = 18;
+        if(x>=0)
+        {
+            System.out.println("Positive");
+        }
+        else
+        System.out.println("Negetive");
+
+    }
+}
