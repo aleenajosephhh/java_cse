39ca58851bb5bf526c50b24cc6e606736dd112f9
aleenajosephhh <aleenajsp123@gmail.com>
2025-03-13 14:14:54 +0530

    it returns the values

---
+++ Return/Return.java
@@ -0,0 +1,12 @@
+public class Return{
+    public int add(int numOne, int numTwo){
+        int numThree = numOne + numTwo;
+        return numThree;
+    }
+    public static void main(String args[])
+    {
+        Return obj = new Return();
+        int c =obj.add(10,20);
+        System.out.println(c);
+    }
+}
