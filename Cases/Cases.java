2d0007a97163e8fbfaba0a27253b206df773836e
aleenajosephhh <aleenajsp123@gmail.com>
2025-03-14 12:11:19 +0530

    we can see yje index

---
+++ Cases/Cases.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Cases {
+   public static void main(String args[]){
+        String name = "hello,world";
+        System.out.println(name.indexOf(","));
+         
+        
+
+    }
+}
