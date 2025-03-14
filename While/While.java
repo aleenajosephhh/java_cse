787d6375d60a604d02ab4f46a0775e8cabb36bec
aleenajosephhh <aleenajsp123@gmail.com>
2025-03-14 10:03:34 +0530

    numbers

---
+++ While/While.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class While{
+    public static void main(String args[]){
+        int i = 92;
+        while(i<99){
+        i++;
+        System.out.println(i);}
+    }
+}
