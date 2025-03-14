0069d278a0bd64d56e9d3cb802d3d49e04580530
aleenajosephhh <aleenajsp123@gmail.com>
2025-03-14 13:47:53 +0530

    this is multilevel programminh

---
+++ Multilevel/Multilevel.java
@@ -0,0 +1,22 @@
+class Parent{
+    public void add(){
+        System.out.println("hello world");
+    }
+}
+class Child extends Parent 
+{
+    void display(){
+        add();
+    }
+}
+class Third extends Child{
+    void helper(){
+        display();
+    }
+}
+public class Multilevel{
+    public static void main (String args[]){
+        Third obj = new Third();
+        obj.helper();
+    }
+    }
