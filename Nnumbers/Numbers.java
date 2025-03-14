67460f32205eb863e0aaac30402652e329376610
aleenajosephhh <aleenajsp123@gmail.com>
2025-03-14 10:56:55 +0530

    :this is my nnumbers

---
+++ Nnumbers/Numbers.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Numbers{
+    public static void main(String args[]){
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter the numbers");
+        int num=input.nextInt();
+        int i=1;
+        while(i<=num)
+        {
+            System.out.println(i);
+            i=i+1;
+        }
+        
+    }
+    }
