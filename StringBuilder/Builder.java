ef16811c32a5d084226b24b0740c2980d738c966
aleenajosephhh <aleenajsp123@gmail.com>
2025-03-15 10:50:57 +0530

    name is given

---
+++ StringBuilder/Builder.java
@@ -0,0 +1,7 @@
+public class Builder{
+    public static void main(String args[]){
+        String s1 = "Aleena";
+        String s2 = " Joseph";
+        System.out.println("name:"+(s1 + s2));
+    }
+}
