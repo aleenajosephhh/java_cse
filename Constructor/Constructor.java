167f4f2b5888596fdf88bc42a0d0686f8ea18275
aleenajosephhh <aleenajsp123@gmail.com>
2025-03-15 11:02:10 +0530

    using constructor

---
+++ Constructor/Constructor.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Constructor {
+    Constructor() {
+        System.out.println("aleena");
+    }
+    public static void main(String args[]) {
+        Constructor obj = new Constructor();
+    }
+}
